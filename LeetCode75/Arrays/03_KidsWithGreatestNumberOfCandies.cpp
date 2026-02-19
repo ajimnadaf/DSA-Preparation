f5574d7b19251f29d156d54b2589149dde41ea01
ajimnadaf <ajimnadaf205@gmail.com>
2026-02-19 21:28:33 +0530

    Day 1: Solved Kids With the Greatest Number of Candies
    Added a new C++ file for the Kids With the Greatest Number of Candies problem, including problem statement and complexity analysis.

---
+++ LeetCode75/Arrays/03_KidsWithGreatestNumberOfCandies.cpp
@@ -0,0 +1,22 @@
+// Problem: Kids With the Greatest Number of Candies
+// Platform: LeetCode 75
+// Approach: Find max element then compare
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+      int maxCandies = 0;
+
+      for(int i = 0; i < candies.size(); i++){
+        if(candies[i] > maxCandies)
+            maxCandies = candies[i];
+      }
+
+      vector<bool> result;
+      for(int i = 0; i < candies.size(); i++){
+        result.push_back(candies[i] + extraCandies >= maxCandies);
+      }
+      return result;
+    }
+};
