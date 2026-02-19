3d66643e85d8bbcac72d26d62ca2133c14987ee6
ajimnadaf <ajimnadaf205@gmail.com>
2026-02-19 14:43:23 +0530

    Day 1: Solved Merge Strings Alternately
    Implemented a function to merge two strings alternately, handling remaining characters.

---
+++ LeetCode75/Arrays/01_MergeStringsAlternately.cpp
@@ -0,0 +1,23 @@
+// Problem: Merge Strings Alternately
+// Platform: LeetCode 75
+// Approach: Two pointers
+// Time Complexity: O(n + m)
+// Space Complexity: O(n + m)
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string result = "";
+        int i = 0;
+
+        while(i < word1.length() && i < word2.length()){
+            result += word1[i];
+            result += word2[i];
+            i++;
+        }
+
+        result += word1.substr(i);
+        result += word2.substr(i);
+
+        return result;
+    }
+};
