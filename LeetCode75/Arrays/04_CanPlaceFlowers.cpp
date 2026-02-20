14430af3afe116da1d2354cf1e0622955140cde8
ajimnadaf <ajimnadaf205@gmail.com>
2026-02-20 20:05:15 +0530

    Day 2: Solved Can Place Flowers (Greedy Approach)

--- LeetCode75/Arrays/04_CanPlaceFlowers.cpp
+++ LeetCode75/Arrays/04_CanPlaceFlowers.cpp
@@ -9,7 +9,7 @@ public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
         int count = 0;
         for (int i = 0; i < flowerbed.size(); i++) {
-            // Check if the current plot is empty.
+           
             if (flowerbed[i] == 0) {
                 // Check if the left and right plots are empty.
                 bool emptyLeftPlot = (i == 0) || (flowerbed[i - 1] == 0);
