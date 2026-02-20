0dbedc817dd272b21ecf915cc050081f4f0aa2e2
ajimnadaf <ajimnadaf205@gmail.com>
2026-02-20 20:34:49 +0530

    Day 3: Solved Reverse Vowels of a String (Two Pointer Approach)
    Implement a solution to reverse the vowels in a string using a two-pointer approach.

---
+++ LeetCode75/Arrays/05_ReverseVowelsOfString.cpp
@@ -0,0 +1,38 @@
+// Problem: Reverse Vowels of a String
+// Platform: LeetCode 75
+// Approach: Two Pointers (Swap vowels from start and end)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+public:
+    bool isVowel(char c) {
+        c = tolower(c);
+        return c == 'a' || c == 'e' || c == 'i' ||  c == 'o' || c == 'u';
+    }
+
+    string reverseVowels(string s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            // Move left pointer until vowel found
+            while (left < right && !isVowel(s[left])) {
+                left++;
+            }
+
+            // Move right pointer until vowel found
+            while (left < right && !isVowel(s[right])) {
+                right--;
+            }
+
+            // Swap vowels
+            swap(s[left], s[right]);
+
+            left++;
+            right--;
+        }
+
+        return s;  
+    }
+};
