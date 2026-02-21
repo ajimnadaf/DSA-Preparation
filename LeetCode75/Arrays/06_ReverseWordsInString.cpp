aa81c578e072156122799afff434da1e78b36fa5
ajimnadaf <ajimnadaf205@gmail.com>
2026-02-21 21:10:47 +0530

    Day 3: Solved Reverse Words in a String (Reverse Traversal Approach)
    Implement reverseWords function to reverse the order of words in a string.

--- LeetCode75/Arrays/06_ReverseWordsInString.cpp
+++ LeetCode75/Arrays/06_ReverseWordsInString.cpp
@@ -3,6 +3,7 @@
 // Approach: Reverse Traversal
 // Time Complexity: O(n)
 // Space Complexity: O(n)
+
 class Solution {
 public:
     string reverseWords(string s) {
@@ -10,7 +11,6 @@ public:
         int i = s.length() - 1;
 
         while(i >= 0) {
-
             while(i >= 0 && s[i] == ' ')
             i--;
 
@@ -18,7 +18,6 @@ public:
                 break;
 
             int j = i;
-
             while(i >= 0 && s[i] != ' ')
             i--;
 
@@ -27,8 +26,6 @@ public:
 
             result += s.substr(i + 1, j - i);
         }
-
         return result;
-
     }
 };
