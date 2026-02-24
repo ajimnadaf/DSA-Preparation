e737d44a9ab2125f006558b9428dbebeafd51833
ajimnadaf <ajimnadaf205@gmail.com>
2026-02-24 21:09:57 +0530

    Day 5: Solved String Compression

---
+++ LeetCode75/Arrays/09_StringCompression.cpp
@@ -0,0 +1,33 @@
+// Problem: String Compression
+// Platform: LeetCode 75
+// Approach: Two Pointers (In-place compression)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+       int n = chars.size();
+       int i = 0;
+       int write = 0;
+
+       while (i < n) {
+        int currentChar = chars[i];
+        int count = 0;
+
+            while (i < n && chars[i] == currentChar){
+                i++;
+                count++;           
+            }
+
+            chars[write++] = currentChar;
+
+            if (count > 1) {
+                string cnt = to_string(count);
+                for (char c : cnt) {
+                    chars[write++] = c;
+                }
+            }
+        }
+        return write;
+    }
+};
