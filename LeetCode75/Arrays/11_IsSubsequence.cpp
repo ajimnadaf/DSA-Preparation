54c78c7eb3de96f657addd0a1f2156dce8f53c49
ajimnadaf <ajimnadaf205@gmail.com>
2026-02-25 22:06:40 +0530

    Update 11_IsSubsequence.cpp

--- LeetCode75/Arrays/11_IsSubsequence.cpp
+++ LeetCode75/Arrays/11_IsSubsequence.cpp
@@ -2,7 +2,9 @@
 // Platform: LeetCode 75
 // Approach: Two Pointers
 // Time Complexity: O(n)
-// Space Complexity: O(1)class Solution {
+// Space Complexity: O(1)
+
+class Solution {
 public:
     bool isSubsequence(string s, string t) {
         int i = 0;
