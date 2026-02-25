a4378b8148078affb3970f81a39632f87bc7ce13
ajimnadaf <ajimnadaf205@gmail.com>
2026-02-25 21:40:41 +0530

    Day 6: Solved Move Zeroes
    Implemented the moveZeroes function using a two-pointer approach to shift non-zero elements forward in the array.

---
+++ LeetCode75/Arrays/10_MoveZeroes.cpp
@@ -0,0 +1,17 @@
+// Problem: Move Zeroes
+// Platform: LeetCode 75
+// Approach: Two Pointers (Shift non-zero elements forward)
+// Time Complexity: O(n)
+// Space Complexity: O(1)class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap (nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+};
