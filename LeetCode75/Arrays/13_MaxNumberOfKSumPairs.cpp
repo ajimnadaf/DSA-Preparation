a5e70ea643983662b33a39bcdc2176141f98f427
ajimnadaf <ajimnadaf205@gmail.com>
2026-02-27 22:34:09 +0530

    Create 13_MaxNumberOfKSumPairs.cpp

---
+++ LeetCode75/Arrays/13_MaxNumberOfKSumPairs.cpp
@@ -0,0 +1,25 @@
+// Problem: Max Number of K-Sum Pairs
+// Platform: LeetCode 75
+// Approach: Hash Map (Count frequency of complements)
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        int count = 0;
+
+        for (int num : nums) {
+            int complement = k - num;
+
+            if (mp[complement] > 0) {
+                count++;
+                mp[complement]--;
+            } 
+            else {
+                mp[num]++;
+            }
+        }
+        return count;
+    }
+};
