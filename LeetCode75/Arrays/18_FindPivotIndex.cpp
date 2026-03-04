aab1e5b376519daefe28e58cd2d409b0722672cd
ajimnadaf <ajimnadaf205@gmail.com>
2026-03-04 22:34:40 +0530

    Create 18_FindPivotIndex.cpp

---
+++ LeetCode75/Arrays/18_FindPivotIndex.cpp
@@ -0,0 +1,31 @@
+// Problem: Find Pivot Index
+// Platform: LeetCode 75
+// Approach: Prefix Sum
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total_sum = 0;
+        
+        // Step 1: Calculate total sum
+        for (int num : nums) {
+            total_sum += num;
+        }
+        
+        int left_sum = 0;
+        
+        // Step 2: Traverse array
+        for (int i = 0; i < nums.size(); i++) {
+            int right_sum = total_sum - left_sum - nums[i];
+            
+            if (left_sum == right_sum) {
+                return i;
+            }
+            
+            left_sum += nums[i];
+        }
+        
+        return -1;
+    }
+};
