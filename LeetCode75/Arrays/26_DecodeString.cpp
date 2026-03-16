6b1801601547afd96e5cf4bd6210dcc542d5aaba
ajimnadaf <ajimnadaf205@gmail.com>
2026-03-16 20:27:19 +0530

    Day 21: Solved Decode String

---
+++ LeetCode75/Arrays/26_DecodeString.cpp
@@ -0,0 +1,50 @@
+// Problem: Decode String
+// Platform: LeetCode 75
+// Approach: Use stack to handle nested encoded patterns
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> countStack;
+        stack<string> stringStack;
+
+        string current = "";
+        int k = 0;
+
+        for(char c : s){
+
+            if(isdigit(c)){
+                k = k * 10 + (c - '0');
+            }
+
+            else if(c == '['){
+                countStack.push(k);
+                stringStack.push(current);
+
+                k = 0;
+                current = "";
+            }
+
+            else if(c == ']'){
+                int repeat = countStack.top();
+                countStack.pop();
+
+                string temp = current;
+
+                current = stringStack.top();
+                stringStack.pop();
+
+                while(repeat--){
+                    current += temp;
+                }
+            }
+
+            else{
+                current += c;
+            }
+        }
+
+        return current;
+    }
+};
